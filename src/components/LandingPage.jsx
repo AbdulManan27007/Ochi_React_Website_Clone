45afa500de841f3c0ac9414e379b13065ba16266
AbdulManan27007 <abdulmanan4066@gmail.com>
2024-02-21 23:30:04 +0500

    update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
     <div className=' w-full h-screen bg-zinc-900 pt-1'>
         <div className='textstructure mt-44 px-20'> {["We Create", "Eye Opening" ,"presentations"].map ((item, index)=>{
             return ( <div className='masker'>
-            <h1 className="uppercase text-[7.5vw] leading-[7vw] tracking-tighter font-semibold font-['Founders_Grotesk']">{item}</h1>
+            <h1 className="uppercase text-[8vw] leading-[7vw] tracking-tighter font-semibold font-['Founders_Grotesk']">{item}</h1>
         </div>)
            
         })}
